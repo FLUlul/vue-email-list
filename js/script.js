6ee9bd816bcc59b6f171b764acbb2c3c4d154b40
FLU <valerio.massj@gmail.com>
2021-11-22 14:42:31 +0100

    extra done, show the emails when they are 10

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ Descrizione:
 Attraverso l’apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email
-e stamparli in pagina all’interno di una lista. (sempre untilizzando Vue)
+e stamparli in pagina all’interno di una lista. (sempre utilizzando Vue)
 
 Bonus:
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
@@ -24,12 +24,10 @@ var app = new Vue(
                 .get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then(function(result){
                     self.email = result.data.response;
-                    console.log(self.email);
 
                     self.arrEmails.push(self.email)
                 });
             }
-            console.log(self.arrEmails); 
         }
     }
 )
